740cb08f737806cc373d9d87975f205662aa8139
bakate <bakateba@gmail.com>
2019-12-25 20:21:47 +0100

    refactored

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import Counting from './components/Couting';
-import store from './reducers';
-import Favoriting from './components/Favoriting';
-
-export const App = () => (
-  <div>
-    <Counting />
-    <Favoriting />
-  </div>
-);
+import App from './components/App';
+import store from './redux/store';
 
 render(
   <Provider store={store}>
